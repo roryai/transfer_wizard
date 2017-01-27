4d5a99aa36ff63c8ee3ce06c5935810180489ed9
Rory Aitchison <rory.aitchison@gmail.com>
2017-01-27 13:56:19 +0000

    user stories written. first attempt to transfer single file using File.rename

---
+++ photo_transfer.rb
@@ -0,0 +1,9 @@
+def single_photo_transfer
+  File.rename /Volumes/Untitled/test/photo1.JPG, /Volumes/Untitled/test/photo1.JPG /Users/rory/Documents/Photo\ test
+end
+
+single_photo_transfer
+
+
+#photo on sd card: /Volumes/Untitled/test/photo1.JPG
+#photo folder on hard drive /Volumes/Untitled/test/photo1.JPG /Users/rory/Documents/Photo\ test
