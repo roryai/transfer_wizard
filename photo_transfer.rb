8f1bb4a1fea1afc10264155e740817a28f49d87c
Rory Aitchison <rory.aitchison@gmail.com>
2017-01-27 16:32:42 +0000

    reads file names and stores them in an array

--- photo_transfer.rb
+++ photo_transfer.rb
@@ -3,3 +3,14 @@ require 'FileUtils'
 def single_photo_transfer
   FileUtils.copy_file('/Volumes/Untitled/test/photo3.JPG', '/Users/rory/Documents/tester/photo3.JPG', preserve = false, dereference = true)
 end
+
+def get_file_names(dir)
+  FileUtils.cd(dir)
+  file_name_array = Dir.glob("*")
+end
+
+def print_file_names(dir)
+  puts get_file_names(dir)
+end
+
+print_file_names('/Users/rory/Documents/tester')
