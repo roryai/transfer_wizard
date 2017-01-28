2227d0184a471a3cab41dfcef04eb24d31269b8d
Rory Aitchison <rory.aitchison@gmail.com>
2017-01-28 08:58:43 +0000

    moved hard coded directories to variables

--- photo_transfer.rb
+++ photo_transfer.rb
@@ -1,5 +1,8 @@
 require 'FileUtils'
 
+camera_dir = "/Volumes/Untitled/test/"
+computer_dir = "/Users/rory/Documents/tester/"
+
 def get_file_names(dir)
   FileUtils.cd(dir)
   file_name_array = Dir.glob("*")
@@ -9,11 +12,11 @@ def single_photo_transfer(copy_from, copy_to)
   FileUtils.copy_file(copy_from, copy_to, preserve = false, dereference = true)
 end
 
-def copy_all_photos
-  photo_list = get_file_names("/Volumes/Untitled/test")
-  photo_list.each do |x|
-    single_photo_transfer("/Volumes/Untitled/test/#{x}", "/Users/rory/Documents/tester/#{x}")
+def copy_all_photos(camera_dir, computer_dir)
+  photo_list = get_file_names(camera_dir)
+  photo_list.each do |file_name|
+    single_photo_transfer(camera_dir + "#{file_name}", computer_dir + "#{file_name}")
   end
 end
 
-copy_all_photos
+copy_all_photos(camera_dir, computer_dir)
