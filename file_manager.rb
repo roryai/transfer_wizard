e24264c50c978919fec02d6cfe80f366e40b98cf
Rory Aitchison <rory.aitchison@gmail.com>
2017-04-03 20:52:07 +0100

    population of file_name_time_array working recursively through directories

--- file_manager.rb
+++ file_manager.rb
@@ -19,12 +19,14 @@ class FileMgr
     # p "file name array at start" + file_name_array.to_s
 
     file_name_array.each do |file_name|
+      p "pwd: " + FileUtils.pwd
         begin
           if EXIFR::JPEG.new(file_name).date_time == nil
             p "no exifr section"
             full_file_path = FileUtils.pwd + "/" + file_name
             file_dir = FileUtils.pwd
             @no_exifr_array << [file_name, "no_time_stamp", full_file_path, file_dir]
+            p file_name + ' added'
           else
             p "exifr section"
             p "exif time: " + (time = EXIFR::JPEG.new(file_name).date_time).to_s
@@ -33,6 +35,7 @@ class FileMgr
             full_file_path = FileUtils.pwd + "/" + file_name
             file_dir = FileUtils.pwd
             @file_name_time_array << [file_name, time, full_file_path, file_dir]
+            p file_name + ' added'
           end
           # if file_name has no EXIF data
         rescue EXIFR::MalformedJPEG
@@ -41,10 +44,15 @@ class FileMgr
           full_file_path = FileUtils.pwd + "/" + file_name
           file_dir = FileUtils.pwd
           @file_name_time_array << [file_name, time, full_file_path, file_dir]
+          p file_name + ' added'
           # if file_name is a directory
         rescue Errno::EISDIR
           p "eisdir- it's a directory error"
-          get_name_time_array(FileUtils.pwd + "/" + file_name)
+          FileUtils.cd(camera_dir + "/" + file_name) do
+            get_name_time_array(FileUtils.pwd)
+          end
+        rescue Errno::ENOENT
+          p "ERRNOENT rescued"
         end
     end
     [@file_name_time_array, @no_exifr_array]
