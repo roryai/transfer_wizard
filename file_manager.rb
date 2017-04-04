e1877675d0147cde6ae1d3094dabf5112c3b99b4
Rory Aitchison <rory.aitchison@gmail.com>
2017-04-04 11:20:55 +0100

    further refactoring of get_name_time_array

--- file_manager.rb
+++ file_manager.rb
@@ -13,56 +13,50 @@ class FileMgr
 
   def get_file_names(dir)
     FileUtils.cd(dir)
-    # line below could cause problems with small files- size limit? Grab by extension?
     file_name_array = Dir.glob("*")
   end
 
   def get_name_time_array(dir)
-    file_name_array = get_file_names(dir)
-
-    file_name_array.each do |file_name|
+    get_file_names(dir).each do |file_name|
+      time = File.ctime(file_name)
       full_file_path = FileUtils.pwd + "/" + file_name
       file_dir = FileUtils.pwd
-
-
-
-          if @pic_extensions.include?(File.extname(file_name)) || @vid_extensions.include?(File.extname(file_name))
-            begin
-              if EXIFR::JPEG.new(file_name).date_time == nil
-                time = File.ctime(file_name)
-                @unsorted_pics_and_vids << [file_name, time, full_file_path, file_dir]
-              else
-                time = EXIFR::JPEG.new(file_name).date_time
-                @file_name_time_array << [file_name, time, full_file_path, file_dir]
-              end
-            rescue EXIFR::MalformedJPEG
-              p 'malformed jpeg'
-              p file_name
-              time = File.ctime(file_name)
-              @unsorted_pics_and_vids << [file_name, time, full_file_path, file_dir]
-            # if file_name is a directory
-            end
-          elsif File.directory?(file_name)
-            FileUtils.cd(dir + "/" + file_name) do
-              get_name_time_array(FileUtils.pwd)
-            end
-          else
-            time = File.ctime(file_name)
-            @unsorted_files << [file_name, time, full_file_path, file_dir]
-          end
-        # # if file_name has no EXIF data
-        # # redundant?
-
-        # rescue Errno::EISDIR
-        #   p "eisdir- it's a directory error"
-        #   FileUtils.cd(dir + "/" + file_name) do
-        #     get_name_time_array(FileUtils.pwd)
-        #   end
-        # rescue Errno::ENOENT
-        #   p "ERRNOENT rescued for file: " + file_name
-
+      file_type_sorter(file_name, time, full_file_path, file_dir, dir)
     end
     [@file_name_time_array, @unsorted_pics_and_vids, @unsorted_files]
+  end
+
+  def file_type_sorter(file_name, time, full_file_path, file_dir, dir)
+    if @pic_extensions.include?(File.extname(file_name)) || @vid_extensions.include?(File.extname(file_name))
+      pic_and_vid_handler(file_name, time, full_file_path, file_dir)
+    elsif File.directory?(file_name)
+      recursive_directory_handler(dir, file_name)
+    else
+      @unsorted_files << [file_name, time, full_file_path, file_dir]
+    end
+  end
+
+  def pic_and_vid_handler(file_name, time, full_file_path, file_dir)
+    begin
+      exif_handler(file_name, time, full_file_path, file_dir)
+    rescue EXIFR::MalformedJPEG
+      @unsorted_pics_and_vids << [file_name, time, full_file_path, file_dir]
+    end
+  end
+
+  def exif_handler(file_name, time, full_file_path, file_dir)
+    if EXIFR::JPEG.new(file_name).date_time == nil
+      @unsorted_pics_and_vids << [file_name, time, full_file_path, file_dir]
+    else
+      time = EXIFR::JPEG.new(file_name).date_time
+      @file_name_time_array << [file_name, time, full_file_path, file_dir]
+    end
+  end
+
+  def recursive_directory_handler(dir, file_name)
+    FileUtils.cd(dir + "/" + file_name) do
+      get_name_time_array(FileUtils.pwd)
+    end
   end
 
 end
